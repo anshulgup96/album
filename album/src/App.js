74078df346208a5b2e1c883b64e8b25295aa3cf8
Anshul Gupta <anshul.gup96@gmail.com>
2020-02-23 02:48:57 +0530

    v1

--- album/src/App.js
+++ album/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 // import Album from './Album';
 import "./App.css";
 
+// Implement pagination
+// Implement search functionality
+
 class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/albums")
@@ -27,28 +30,32 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className='top-navbar'>
+        <div className="top-navbar">
           <h1>Albums List</h1>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control search-album" type="search" placeholder="Search for an album"/>
+            <input
+              class="form-control search-album"
+              type="search"
+              placeholder="Search for an album"
+            />
           </form>
         </div>
-        <div className='container'>
-        {this.state.albumList.map((album, i) => (
-          <div className="card" key={i + 1}>
-            <div className="card-body">
-              <h5 className="card-title">{album.title}</h5>
-              <p className="card-text">Album Id: {album.id}</p>
-              <p className="card-text">Album Id: {album.id}</p>
-              <p
-                className="btn btn-primary"
-                onClick={() => this.openAlbum(i)}
-              >
-                View Album
-              </p>
+        <div className="container">
+          {this.state.albumList.map((album, i) => (
+            <div className="card" key={i + 1}>
+              <div className="card-body">
+                <h5 className="card-title">{album.title}</h5>
+                <p className="card-text">Album Id: {album.id}</p>
+                <p className="card-text">Album Id: {album.id}</p>
+                <p
+                  className="btn btn-primary"
+                  onClick={() => this.openAlbum(i)}
+                >
+                  View Album
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
       </div>
     );
