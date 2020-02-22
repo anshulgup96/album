a3c44bf52fef2ae13362021281fd7fd0b895fce3
Anshul Gupta <anshul.gup96@gmail.com>
2020-02-22 22:20:25 +0530

    api response for album number working

--- album/src/Album.js
+++ album/src/Album.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class Album extends Component{
-    constructor(props){
-        super(props);
-    }
-    
-    render(){
-        return(
-            <div>
-                <h1>List of albums</h1>
-                {this.state.list.map(item => 
-                )}
-            </div>
-        )
-    }
+export default class Album extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <th scope="col">UserID</th>
+          <th scope="col">Album ID</th>
+          <th scope="col">Album Name</th>
+          {this.state.albumList.map(album => (
+            <tr
+              className="album-card"
+              key={album.id}
+              onClick={() => console.log("hello")}
+            >
+              <td>{album.userId}</td>
+              <td>{album.id}</td>
+              <td>{album.title}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
 }
--- album/src/App.js
+++ album/src/App.js
@@ -1,42 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Album from './Album';
-import './App.css'
+import "./App.css";
 
-  class App extends Component {
-    componentWillMount() {
-      fetch('https://jsonplaceholder.typicode.com/albums')
+class App extends Component {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/albums")
       .then(res => res.json())
-      .then((data) => {
-        this.setState({ albumList: data })
-      })
-      .catch(console.log);
-    }
+      .then(data => {
+        this.setState({ albumList: data });
+      });
+  }
 
-    state = {
-      albumList: []
-    }
+  openAlbum(key) {
+    key = key + 1;
+    fetch("https://jsonplaceholder.typicode.com/albums/" + key + "/photos")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
 
-    render() {
-      console.log(this.state.albumList);
-      return (
-        <div className="container">
-          <h1>Albums</h1>
-          <table className="table">
-          <th scope='col'>UserID</th>
-          <th scope='col'>Album ID</th>
-          <th scope='col'>Album Name</th>
-            {this.state.albumList.map(album => 
-              <tr className='album-card' key={album.id}>
+  state = {
+    albumList: []
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Albums</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">UserID</th>
+              <th scope="col">Album ID</th>
+              <th scope="col">Album Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.albumList.map((album, i) => (
+              <tr
+                className="album-card"
+                key={i + 1}
+                onClick={() => this.openAlbum(i)}
+              >
                 <td>{album.userId}</td>
                 <td>{album.id}</td>
                 <td>{album.title}</td>
               </tr>
-            )}
-          </table>
-        </div>
-      );
-      
-    }
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
+}
 
-  export default App;
+export default App;
